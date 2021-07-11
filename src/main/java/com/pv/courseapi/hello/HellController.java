fa4fffb0710c75579b89eaa0ae9a342d674faf08
pavan tiruvuri <pavan.tiruvuri@oracle.com>
2021-07-11 18:41:34 +0530

    Added a simple rest controller at path '/hello'

---
+++ src/main/java/com/pv/courseapi/hello/HellController.java
@@ -0,0 +1,13 @@
+package com.pv.courseapi.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellController {
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "Hi";
+    }
+}
